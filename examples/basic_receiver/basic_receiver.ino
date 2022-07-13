103b1511147b071349826efa49f3d5146a31bcad
Matthew Bellafaire <matthew.bellafaire@gmail.com>
2022-07-13 17:16:09 -0400

    added basic examples

---
+++ examples/basic_receiver/basic_receiver.ino
@@ -0,0 +1,90 @@
+/* basic_receiver.ino
+ *  
+ *  initializes an ESP32 as a receiver and prints the received button inputs
+ *  to the serial terminal so that we can view them. Demonstrates all the controller
+ *  inputs that can be read remotely. 
+ */
+
+
+#include <makers_controller.h>
+
+//create receiver object that is going to be our way to get data
+MakersController receiver = MakersController();
+
+void setup() {
+  //enable serial 
+  Serial.begin(115200);
+
+  //start the receiver, this opens the device up to begin receiving data from a controller
+  //this is the only bit of startup code required, any controller that desires to connect to the 
+  //MAC address of this ESP32 will automatically do so. 
+  receiver.startReceiver();
+}
+void loop() {
+
+  //Read joysticks and print to serial terminal
+  //right joystick
+  Serial.print("Right Joystick ");
+  Serial.print(receiver.readRightJoystickX());
+  Serial.print(",");
+  Serial.println(receiver.readRightJoystickY());
+
+  //left joystick
+  Serial.print("Left Joystick ");
+  Serial.print(receiver.readLeftJoystickX());
+  Serial.print(",");
+  Serial.println(receiver.readLeftJoystickY());
+
+  //now lets read all the buttons, see github page for placement of switches
+  //on the receiver
+  Serial.print("Switches: ");
+
+  //left hand up
+  Serial.print(receiver.readSW1());
+  Serial.print(" ");
+
+  //left hand right
+  Serial.print(receiver.readSW2());
+  Serial.print(" ");
+
+  //left hand down
+  Serial.print(receiver.readSW3());
+  Serial.print(" ");  
+
+  //left hand left 
+  Serial.print(receiver.readSW4());
+  Serial.print(" ");
+
+  //right hand up 
+  Serial.print(receiver.readSW5());
+  Serial.print(" ");
+
+  //right hand right
+  Serial.print(receiver.readSW6());
+  Serial.print(" ");
+
+  //right hand down 
+  Serial.print(receiver.readSW7());
+  Serial.print(" ");
+
+  //right hand left
+  Serial.print(receiver.readSW8());
+  Serial.print(" ");
+
+  //read trigger buttons
+  Serial.print(receiver.readRightTrigger());
+  Serial.print(" ");
+
+  Serial.print(receiver.readLeftTrigger());
+  Serial.print(" ");
+
+  //joystick switches
+  Serial.print(receiver.readRightJoystickSwitch());
+  Serial.print(" ");
+
+  Serial.print(receiver.readLeftJoystickSwitch());
+  Serial.println(); //done reading switches
+
+  //delay for this example, but you can call these functions as fast as you would like
+  delay(250);
+}
---
+++ examples/basic_transmitter/basic_transmitter.ino
@@ -0,0 +1,30 @@
+/* basic_receiver.ino
+ *  
+ *  initializes a makers controller with the most basic functionality and sends 
+ *  controller data to a connected ESP32. 
+ */
+
+//import library
+#include <makers_controller.h>
+
+//create controller object
+MakersController controller = MakersController(); 
+
+void setup() {
+  //init serial
+  Serial.begin(115200);
+
+  //In order to connect to a device with the controller the controller 
+  //must have the device's MAC address, when startReceiver() is called for the target device
+  //the MAC address will be printed to the serial terminal, that can be coppied here. 
+  //the MAC address should be in a similar format to below 
+  String targetDeviceMAC = "94:B9:7E:C4:A4:CC";
+
+  //start the controller and give it the address of the target 
+  controller.startController(targetDeviceMAC);
+}
+void loop() {
+  //read the buttons and the joysticks then send that to 
+  //the target device. 
+  controller.readAndSend();
+}
